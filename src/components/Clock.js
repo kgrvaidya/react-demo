130f8be6423636ba5a2f3574bcbff4a88f6d9a76
K Ramakrishna Vaidya <kgrvaidya@gmail.com>
2019-08-27 17:57:08 +0530

    Click listner added

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react'
+// import variables from '../scss/_variables.scss'
+// import style from '../style.scss'
 
 class Clock extends Component {
     constructor(props){
         super(props);
 
         this.state = {
-            date : new Date()
+            date : new Date(),
+            isDayMode : false
         }
 
+        this.onClick = this.onClick.bind(this);
         
     }
 
@@ -24,11 +28,28 @@ class Clock extends Component {
             date : new Date()
         })} ,1000)
     }
+ 
+    onClick(e){ 
+        let divi = document.querySelector('body');
+
+        if (this.state.isDayMode == false){
+            divi.classList.add('night');
+            divi.classList.remove('day');
+        }
+        else {
+            divi.classList.add('day');
+            divi.classList.remove('night');
+        }
+        this.setState({isDayMode : !this.state.isDayMode})
+
+    }
 
     render() {
         return (
-            <div>
-                <h1>Current time is {this.state.date.toLocaleTimeString()}</h1>                
+            <div id="clock">
+                <h1>Current time is {this.state.date.toLocaleTimeString()}</h1>
+                <button id="mode-button" onClick = {this.onClick}>{ this.state.isDayMode ? 'Click for Day' : 'Click for Night'}</button>                
+                <p>{this.state.isDayMode}</p>
             </div>
         )
     }
